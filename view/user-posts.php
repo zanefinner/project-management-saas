c1c60e5279c187a1cac7a171ba7fc9c838e65c5c
Zane Finner <zanefinnergm@gmail.com>
2020-03-31 21:27:24 -0400

    first init

---
+++ view/user-posts.php
@@ -0,0 +1,17 @@
+<?php require 'partials/header.php';?></head><body>
+<?php require 'partials/nav.php'?>
+
+<div class="container">
+  <div class="row">
+    <?php require 'partials/user-profile-col.php'?>
+    <div class="posts-col col-12 col-sm-12 col-md-12 col-lg-6">
+
+      <h1>Posts By asdfgh></h1>
+
+
+
+    </div>
+    <?php require 'partials/discover-col.php'?>
+  </div>
+</div>
+<?php require 'partials/footer.php';?>
